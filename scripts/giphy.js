d16bda945dbfd943ca1a7c31379db234f3b01e7f
Deiby Toralva <deiby@devacademy.la>
2016-04-19 19:45:59 -0500

    Add semicolons

--- scripts/giphy.js
+++ scripts/giphy.js
@@ -10,12 +10,12 @@ function spotify(bot, controller, message) {
     if (!err && res.statusCode === 200) {
       var response = JSON.parse(body);
       var gifs = response.data.length;
-      var urls = response.data[0].images.fixed_height.url
+      var urls = response.data[0].images.fixed_height.url;
       if (gifs > 0 && urls !== null) {
         bot.reply(message, urls);
       } else {
         msg = 'Huh... I couldn\'t find `' + message.match[1] + '` on Giphy.';
-        bot.reply(message, msg)
+        bot.reply(message, msg);
       }
     } else {
       msg = 'Huh... I couldn\'t find `' + message.match[1] + '` on Giphy.';
@@ -27,7 +27,7 @@ function spotify(bot, controller, message) {
 module.exports = {
   name: 'giphy me',
   author: 'Deiby Toralva (deibytb)',
-  patterns: ['giphy me (.*)'],
+  //patterns: ['giphy me (.*)'],
   types: ['direct_message', 'direct_mention'],
   description: 'Search for a gif on Giphy',
   command: giphy,
