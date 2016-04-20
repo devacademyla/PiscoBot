530e5f21f3a48ea91f8cd3aef0817bda5b3028a0
Deiby Toralva <deiby@devacademy.la>
2016-04-19 19:37:09 -0500

    Config test

--- tests/scripts/giphy.test.js
+++ tests/scripts/giphy.test.js
@@ -3,8 +3,8 @@ var assert = require('chai').assert;
 var helper = require(__dirname + '/../test.helper.js');
 var spotify = require(__dirname + '/../../scripts/giphy.js');
 
-describe('spotify', function() {
+describe('giphy', function() {
   it('should have metaData', function() {
-    helper.metaDataCheck(assert, spotify);
+    helper.metaDataCheck(assert, giphy);
   });
 });
