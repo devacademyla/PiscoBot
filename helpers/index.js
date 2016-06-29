ab174d836da2b8de3e0bf9de6c90ec18255453dd
Daniel Gallegos <daniel@tacowolf.net>
2016-06-29 16:01:03 -0500

    Out with the old

--- helpers/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-function randomFromArray(array) {
-  var random = array[Math.floor(Math.random() * array.length)];
-  return random;
-}
-module.exports = {
-  randomFromArray: randomFromArray,
-};
