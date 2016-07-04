77a958f8bb980a889e13c20d05bbfa64dbcb1124
Daniel Gallegos <danie@tacowolf.net>
2016-07-03 21:28:05 -0500

    Add name storage to hello function

--- modules/core/hello.js
+++ modules/core/hello.js
@@ -5,8 +5,32 @@
 
 /* eslint-disable */
 piscobot.hears('hello', ['direct_message', 'direct_mention', 'mention'],
-/* eslint-enable */
+    /* eslint-enable */
     function(bot, message) {
-        bot.reply(message, 'Hello yourself, ' + message.user + '.');
+        piscobot.storage.users.get(message.user, function(err, user) {
+            if (user && user.name) {
+                bot.api.reactions.add({
+                    timestamp: message.ts,
+                    channel: message.channel,
+                    name: 'smile',
+                }, function(err) {
+                    if (err) {
+                        bot.botkit.log('Failed to add emoji reaction :(', err);
+                    }
+                });
+                bot.reply(message, 'Hey, there, ' + user.name + '! :smile:');
+            } else {
+                bot.api.reactions.add({
+                    timestamp: message.ts,
+                    channel: message.channel,
+                    name: 'open_mouth',
+                }, function(err) {
+                    if (err) {
+                        bot.botkit.log('Failed to add emoji reaction :(', err);
+                    }
+                });
+                bot.reply(message, 'Oh, hello! :sweat_smile:');
+            }
+        });
     }
 );
