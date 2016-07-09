5fd6ab4c35592d3f25abb24588dc4d5d9317bd0f
Daniel Gallegos <daniel@tacowolf.net>
2016-07-08 19:13:27 -0500

    Add blank example template

---
+++ modules/example/blank.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+/*
+// PiscoBot Script
+
+var commandDescription = {
+  name: '',
+  author: '',
+  trigger: '',
+  version: 1.0,
+  description: '',
+  module: ''
+};
+
+global.botHelp.push(commandDescription);
+
+global.piscobot.hears('', [''],
+  function(bot, message) {
+    bot.reply(message, '');
+  }
+); 
+*/
+/* eslint-enable */
