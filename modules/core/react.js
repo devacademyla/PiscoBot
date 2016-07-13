ba80a22ef4d3bab1b31a145bd5fc624c2c52bc7c
Daniel Gallegos <daniel@tacowolf.net>
2016-07-13 18:55:09 -0500

    Add titular reaction 🍹

--- modules/core/react.js
+++ modules/core/react.js
@@ -3,12 +3,28 @@
 // This script is special! It's where you add things that the bot should react to in chat with 
 // emotes. It's not added to the help file so it doesn't clutter up the rest of the commands.
 
-global.piscobot.hears(['lilmoises', 'lilmoises69'], ['ambient'],
+global.piscobot.hears(['lilmoises', 'lilmoises69'], 
+  ['ambient', 'direct_message', 'direct_mention', 'mention'],
   function(bot, message) {
     bot.api.reactions.add({
       timestamp: message.ts,
       channel: message.channel,
       name: 'violin'
+    }, function(err) {
+      if(err) {
+        bot.botkit.log('Failed to add emoji reaction :(', err);
+      }
+    });
+  }
+);
+
+global.piscobot.hears(['pisco', 'hangover', 'drunk', 'alcohol'], 
+  ['ambient', 'direct_message', 'direct_mention', 'mention'],
+  function(bot, message) {
+    bot.api.reactions.add({
+      timestamp: message.ts,
+      channel: message.channel,
+      name: 'tropical_drink'
     }, function(err) {
       if(err) {
         bot.botkit.log('Failed to add emoji reaction :(', err);
