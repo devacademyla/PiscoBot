abc3b5814107c026bf68417fa67c92278c24929e
Daniel Gallegos <daniel@tacowolf.net>
2016-07-13 18:45:03 -0500

    Add reaction scripts

---
+++ modules/core/react.js
@@ -0,0 +1,18 @@
+// PiscoBot Script
+
+// This script is special! It's where you add things that the bot should react to in chat with 
+// emotes. It's not added to the help file so it doesn't clutter up the rest of the commands.
+
+global.piscobot.hears(['lilmoises', 'lilmoises69'], ['ambient'],
+  function(bot, message) {
+    bot.api.reactions.add({
+      timestamp: message.ts,
+      channel: message.channel,
+      name: 'violin'
+    }, function(err) {
+      if(err) {
+        bot.botkit.log('Failed to add emoji reaction :(', err);
+      }
+    });
+  }
+);
