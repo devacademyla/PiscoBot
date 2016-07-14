1a0b40b76099d9db8548c3f3c2c26a06b484bef3
Daniel Gallegos <daniel@tacowolf.net>
2016-07-13 19:47:13 -0500

    (ﾉಥ益ಥ）ﾉ﻿ ┻━┻

---
+++ modules/fun/table_flip.js
@@ -0,0 +1,40 @@
+// PiscoBot Script
+
+var commandDescription = {
+  name: 'Table Flip',
+  author: 'Daniel Gallegos [@that_taco_guy]',
+  trigger: 'table flip',
+  version: 1.0,
+  description: '(╯°□°）╯︵ ┻━┻',
+  module: 'Fun'
+};
+
+global.botHelp.push(commandDescription);
+
+var _ = require('underscore');
+
+global.piscobot.hears(['table(flip| flip)', 'flip( | a )table'], 
+  ['direct_mention', 'direct_message'],
+  function(bot, message) {
+    var flips = [
+      '(╯°□°）╯︵ ┻━┻',
+      '┬─┬﻿ ノ( ゜-゜ノ)',
+      '(ノ ゜Д゜)ノ ︵ ┻━┻',
+      '(╯°□°)╯︵ ┻━┻ ︵ ╯(°□° ╯)',
+      '┬─┬﻿ ︵ /(.□. \\）',
+      '‎(ﾉಥ益ಥ）ﾉ﻿ ┻━┻',
+      '(ノ^_^)ノ┻━┻ ┬─┬ ノ( ^_^ノ)',
+      '(╯°Д°）╯︵ /(.□ . \\)',
+      '(╯\'□\')╯︵ ┻━┻',
+      '(ﾉಥДಥ)ﾉ︵┻━┻･/',
+      '(/ .□.)\\ ︵╰(゜Д゜)╯︵ /(.□. \\)',
+      '(._.) ~ ︵ ┻━┻',
+      'ʕノ•ᴥ•ʔノ ︵ ┻━┻',
+      '(/¯◡ ‿ ◡)/¯ ~ ┻━┻',
+      '(/¯◡ ‿ ◡)/¯ ~ ┻━┻',
+      '┻━┻ ︵ ლ(⌒-⌒ლ)',
+      'ʎɯǝpɐɔɐ ︵ヽ(`Д´)ﾉ︵ ʌǝp'
+    ];
+    bot.reply(message, _.sample(flips));
+  }
+);
